92025e1687074d44cb525f6a4c84bd8a0b7c0427
Kavya25007 <kavya563700@gmail.com>
2026-05-03 18:02:09 +0530

    added a new file

---
+++ C++/Basic/sum_two_no_.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a , b;
+    cout << "Enter the value of a"<< endl;
+    cin >> a;
+    cout <<  "The value  of a is "<<a<<endl;
+
+    cout << "Enter the value of b :"<< endl;
+    cin >> b;
+    cout << "The value of b is "<< b << endl;
+
+    int sum = a+b;
+    cout << "Sum = "<< (a+b) << endl;
+    return 0;
+}
+
