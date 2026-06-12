d1f1cf8a90ac97431cc506ab25ccc9607b662a71
Kavya25007 <kavya563700@gmail.com>
2026-06-12 11:01:37 +0530

    array.cpp

--- Array/Basic_Array/array.cpp
+++ Array/Basic_Array/array.cpp
@@ -1,17 +1,13 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    int marks[5] = { 100, 76 , 98 , 90 , 87};
-    marks[2] = 99;
-
-    cout<<marks[0]<<endl;
-    cout<<marks[1]<<endl;
-    cout<<marks[2]<<endl;
-    cout<<marks[3]<<endl;
-    cout<<marks[4]<<endl;
-
+    int marks[5] = { 98 , 87 , 59 , 79 , 82};
+    marks[3] = 91;
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
 
     return 0;
-
 }
