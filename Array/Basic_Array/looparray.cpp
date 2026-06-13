1325bfcd6fbd2cd30c1fc27a21d0eba60b74d8bb
Kavya25007 <kavya563700@gmail.com>
2026-06-13 22:25:52 +0530

    looparray.cpp

--- Array/Basic_Array/looparray.cpp
+++ Array/Basic_Array/looparray.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
-
-int main() {
-    int marks[5] = {10, 49, 98, 37, 52};
+int main(){
     int size = 5;
-
-    for(int i = 0; i < size; i++) {
+    int marks[size];
+    for(int i = 0; i < size; i++){
+        cin >> marks[i];
+    }
+    for(int i = 0; i < size; i++){
         cout << marks[i] << endl;
     }
-
     return 0;
 }
