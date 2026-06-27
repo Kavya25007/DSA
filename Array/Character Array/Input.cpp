661fd1bafb11940d12baf7c88d216c52360c3e9e
Kavya25007 <kavya563700@gmail.com>
2026-06-27 20:22:40 +0530

    Input.cpp

---
+++ Array/Character Array/Input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char str[100];
+
+    cout << "Enter the word = "<< endl;
+    cin.getline(str , 100);
+
+    cout << "Output = "<<str<< endl;
+
+    return 0;
+}
