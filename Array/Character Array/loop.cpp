30611b7dde899a620d3e3dbca1ea71aa74335f9b
Kavya25007 <kavya563700@gmail.com>
2026-06-28 16:57:28 +0530

    Commit

---
+++ Array/Character Array/loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+    char str[] ="Kavya Kushwaha";
+
+    int len = 0;
+    for(int i = 0; str[i] != '\0'; i++){
+        len++;
+
+    }
+
+    cout << "Length of string = "<< len<< endl;
+    return 0;
+
+ }
