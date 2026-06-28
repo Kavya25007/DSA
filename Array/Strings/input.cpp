6006c070e245c473d25676734dc079ea2fbf6eed
Kavya25007 <kavya563700@gmail.com>
2026-06-28 16:56:42 +0530

    Commit

---
+++ Array/Strings/input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+ int main(){
+
+    string str;
+    cout << " Enter String = "<< endl;
+
+    getline(cin , str);
+
+    cout << "String Output = "<< str<< endl;
+    return 0;
+
+ }
