d119fae6b62aa764ddbca2b6b4c56593a0467dba
Kavya25007 <kavya563700@gmail.com>
2026-07-08 11:40:26 +0530

    Commit

---
+++ Mathematics/Palindrome.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int revNum(int n){
+    int revNum = 0;
+    while(n != 0){
+        int digit = n % 10;
+
+        if(revNum > INT8_MAX/10 || revNum < INT8_MIN /10){
+            return 0;
+        }
+
+        revNum = (revNum * 10) + digit;
+        n = n / 10;
+    }
+    return revNum;
+}
+
+int main(){
+
+    int n = 1231;
+    int reverse = revNum(n);
+    
+    if(n == reverse){
+        cout << "Pallindrome"<< endl;
+    }
+    else{
+        cout << "Non Pallindrome"<< endl;
+    }
+    return 0;
+}
+
