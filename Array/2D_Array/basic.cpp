0a50717e7742a24fd4ec69a1fa8307c49e61ae72
Kavya25007 <kavya563700@gmail.com>
2026-07-08 12:04:53 +0530

    Commit

---
+++ Array/2D_Array/basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int matrix[2][3] = {{1,2,3} , {4,5,6}};
+    int rows = 2;
+    int columns = 3;
+
+
+    cout << matrix[1][2]<< endl;
+    return 0;
+
+}
